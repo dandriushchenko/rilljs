7969745c934a1e3dabfc8232cfca29c3416ab8fc
oneznamov <oneznamov@playq.net>
2026-03-06 15:49:05 +0200

    fix: actions

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       include: ['src/model/**/*.ts', 'src/library/**/*.ts'],
+      reporter: ['text', 'json-summary'],
     },
     environment: 'node',
   },
