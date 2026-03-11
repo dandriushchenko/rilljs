4cb764ca5c982245ce33617c7b75da3c9d73a09e
oneznamov <oneznamov@playq.net>
2026-03-11 11:40:57 +0200

    update: config

--- vite.config.ts
+++ vite.config.ts
@@ -4,31 +4,36 @@ import dts from 'vite-plugin-dts';
 import { resolve } from 'path';
 
 export default defineConfig(({ mode }) => {
-  const isDemo = mode === 'demo';
+    const isDemo = mode === 'demo';
 
-  return {
-    base: isDemo ? '/rilljs/' : '/',
-    plugins: [react(), ...(!isDemo ? [dts({ tsconfigPath: './tsconfig.app.json', rollupTypes: true })] : [])],
-    build: {
-      outDir: isDemo ? 'dist-demo' : 'dist',
-      ...(isDemo
-        ? {}
-        : {
-            lib: {
-              entry: resolve(__dirname, 'src/index.ts'),
-              name: 'RillJS',
-              fileName: (format) => `rilljs.${format}.js`,
-            },
-            rollupOptions: {
-              external: ['react', 'react-dom'],
-              output: {
-                globals: {
-                  react: 'React',
-                  'react-dom': 'ReactDOM',
-                },
-              },
-            },
-          }),
-    },
-  };
+    return {
+        base: isDemo ? '/rilljs/' : '/',
+        plugins: [react(), ...(!isDemo ? [dts({ tsconfigPath: './tsconfig.app.json', rollupTypes: true })] : [])],
+        build: {
+            outDir: isDemo ? 'dist-demo' : 'dist',
+            ...(isDemo
+                ? {}
+                : {
+                    lib: {
+                        entry: resolve(__dirname, 'src/index.ts'),
+                        name: 'RillJS',
+                        fileName: (format) => `rilljs.${format}.js`,
+                    },
+                    rollupOptions: {
+                        external: [
+                            'react',
+                            'react-dom',
+                            'react/jsx-runtime',
+                            'react/jsx-dev-runtime'
+                        ],
+                        output: {
+                            globals: {
+                                react: 'React',
+                                'react-dom': 'ReactDOM',
+                            },
+                        },
+                    },
+                }),
+        },
+    };
 });
